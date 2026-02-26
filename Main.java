00ba1911416a978f493b890fa3e329c0e6ba7cbf
Alexey <alexeydruz24@gmail.com>
2026-02-26 19:34:07 +0700

    Исправлен отступ в вступительном сообщении

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final var scanner = new Scanner(System.in);
-        System.out.print("Представтесь:");
+        System.out.print("Представтесь: ");
         final var name = scanner.next();
         System.out.println("Привет, " + name);
     }
